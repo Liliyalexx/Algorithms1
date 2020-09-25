8223070bbad272382f5c15245bb4a87cda5939a6
Liliyalexx <liliya.fedyurina@gmail.com>
2020-09-25 20:46:19 +0000

    Initial commit

---
+++ Javascript Algorithms/Binary_Search.js
@@ -0,0 +1,24 @@
+function binarySearch(arr, elem){
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2)
+  console.log(start, middle,end);
+}
+
+while( arr[middle] !== elem) {
+
+  if (elem <arr[middle]){
+    end = middle -1;
+  } else {
+    start = middle +1;
+  }
+  middle = Math.floor((start + end) / 2)
+}
+console.log (start, middle, end);
+if (arr[middle] == elem) {
+return middle;
+  }
+  return -1;
+
+
+binarySearch([2,5,6,9,13,15,, 30], 15)
